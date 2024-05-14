d21160f1e30478dc3fec7802f90e06bb0959963c
jasonzshi <jasonzshi@yahoo.com>
2024-05-13 22:44:59 -0500

    datalog working

--- PotCode.cpp
+++ PotCode.cpp
@@ -15,8 +15,14 @@
 #include "extern/hx711-pico-c/include/hx711.h"
 #include "extern/hx711-pico-c/include/hx711_noblock.pio.h"
 
+// sd libs
+#include "ff.h"
+#include "sd_card.h"
+#include "hw_config.h"
+#include "diskio.h" //???
+//#include "sd_spi.h"
+#include "f_util.h"
 //ml libs
-
 
 //#include "model-parameters/model_metadata.h"
 //#include "edge-impulse-sdk/dsp/numpy_types.h" // dunno if needed
@@ -46,32 +52,37 @@ bool nowater =false; // bool for if water reservoir is empty
 //scale init
 hx711_t hx;
 scale_t sc; // scale lib
+// weight calc
+mass_t maxweightstruct; // struct for maxweight
 
+/*
 
-void on_pwm_wrap() {
-   // static int gpiopin = 14; // made global
-    static int fade = 120;
-    static bool going_up = true;
-    // Clear the interrupt flag that brought us here
-    pwm_clear_irq(pwm_gpio_to_slice_num(gpiopin));
-
-    if (going_up) {
-        ++fade;
-        if (fade > 255) {
-            fade = 255;
-            going_up = false;
-        }
-    } else {
-        --fade;
-        if (fade < 120) {
-            fade = 120;
-            going_up = true;
-        }
+static spi_t spis[] = {  // One for each SPI.
+    {
+        .hw_inst = spi0,  // SPI component
+        .miso_gpio = 4, // GPIO number (not pin number)
+        .mosi_gpio = 3,
+        .sck_gpio = 2,
+        .baud_rate = 12500 * 1000,  
+        //.baud_rate = 25 * 1000 * 1000, // Actual frequency: 20833333. 
     }
-    // Square the fade value to make the LED's brightness appear more linear
-    // Note this range matches with the wrap value
-    pwm_set_gpio_level(gpiopin, fade * fade);
-}
+};
+
+// Hardware Configuration of the SD Card "objects"
+static sd_card_t sd_cards[] = {  // One for each SD card
+    {
+        .pcName = "0:",   // Name used to mount device
+        .spi = &spis[0],  // Pointer to the SPI driving this card
+        .ss_gpio = 5,    // The SPI slave select GPIO for this SD card
+        .use_card_detect = false,
+        //.card_detect_gpio = 13,   // Card detect
+        //.card_detected_true = -1  // What the GPIO read returns when a card is
+                                 // present. Use -1 if there is no card detect.
+    }};
+
+*/
+
+
 
 // sensor interrupt
 void senseitr(uint , uint32_t) // change to cpp vers
@@ -79,12 +90,12 @@ void senseitr(uint , uint32_t) // change to cpp vers
  //gpio_put(15,1);   // turn off motor
  gpio_put(14,0);
  gpio_put(20,0); // turn off amber led
+ printf("levelsense Triggered");
 }
 
 
 
-// weight calc
-mass_t maxweightstruct; // struct for maxweight
+
 
 
 // ML MODEL
@@ -151,6 +162,7 @@ double val;
         mass.ug = mass.ug /1000000; // wonky divider to make values match
         datain = mass.ug;
         }
+    bool initweightset = false; // bool for if a weight is set
     while(mass.ug < 10 && t.sec < 30 ){ // while still under min threshold and less than 30 seconds
 
         if(scale_weight(&sc, &mass, &opt)) { // gets weight again
@@ -184,13 +196,38 @@ double val;
                 mass.ug = mass.ug /1000000; // wonky divider to make values match
                 datain = mass.ug;
                 }
+            initweightset = true;
             if (maxweightstruct.ug < mass.ug) // just in case user lifts off object in those 300 ms
             {
                 maxweightstruct.ug = mass.ug;
+                //initweightset = false; // double check if this logic works 
             }
+            
         }
         //blink
+            
+            
     }
+    printf("initmaxweight: %f\n", maxweightstruct.ug);
+    while(t.sec < 30 && initweightset == true) // adds grace period to pick up if user waters after putting the pot down
+            {
+               if(scale_weight(&sc, &mass, &opt)) { // gets weight
+                double val;
+                mass_get_value(&mass, &val);
+                char buff[MASS_TO_STRING_BUFF_SIZE];
+                mass.ug = mass.ug * -1; // invert values
+                mass_to_string(&mass, buff); 
+                //printf("%s\n", buff);
+                mass.ug = mass.ug /1000000; // wonky divider to make values match
+                datain = mass.ug;
+                }
+                rtc_get_datetime(&t);
+                sleep_ms(100);
+                if (mass.ug < maxweightstruct.ug * 1.33 && mass.ug > maxweightstruct.ug) // 33% increase tolerance
+                {
+                   maxweightstruct.ug = mass.ug; 
+                }
+            }
     //lock maxweight
     gpio_put(20,0); // turn off amber led
     printf("final maxweight: %f\n", maxweightstruct.ug);
@@ -199,28 +236,74 @@ double val;
     totalwateravailable.ug = totalwateravailable.ug/1000000; // weird scaling thing again
     //twoweights bothweights= {mass,maxweightstruct}; // pass mass values into weight struct
     //xTaskCreate(BLedhandler,"BlueLEDTask",128,&bothweights,2,NULL); //thing to handle blue led
+
+   // xTaskCreate(drymotorstopper,"DryStop",configMINIMAL_STACK_SIZE, &params, 1, NULL);
 }
 
+//haha
 
+/* might not need
+// Scale params to be passed to RTOS Function
+struct ScaleParams{
+    scale_t passsc; // passed scale object
+    scale_options_t passopt; //passed options
+    mass_t passmass; // mass passed 
+};
+// RTOS get weight function dunno if I should do this
 
-// blue led toggle for when target weight is hit
-void BLedhandler(mass_t mass, mass_t maxweightstruct)
+void getweight (void *params) 
 {
-    if (mass.ug >= maxweightstruct.ug)
+   ScaleParams *passedparams = static_cast<ScaleParams*>(params);
+
+}
+*/
+// RTOS function for monitoring and stopping motor if it is dry
+void drymotorstopper (void *params) // check if datetime is correct
+{
+    static bool IsSetup = false;
+    if (!IsSetup) // single run setup loop
     {
-        gpio_put(21,1); // turn on blue led
+        IsSetup = true;
     }
-    else
+
+    mass_t dryloopmass;
+    scale_options_t opt = SCALE_DEFAULT_OPTIONS;
+    mass_t initweight; 
+   //Poll Weight
+   
+   if(scale_weight(&sc, &dryloopmass, &opt)) { // gets weight again 
+        double val;
+        mass_get_value(&dryloopmass, &val);
+        char buff[MASS_TO_STRING_BUFF_SIZE];
+        dryloopmass.ug = dryloopmass.ug * -1; // invert values
+        mass_to_string(&dryloopmass, buff);
+        dryloopmass.ug = dryloopmass.ug /1000000; // wonky divider to make values match
+        }
+    // establish init weight
+   if (initweight.ug) // checks if has value and, if not, assigns one
+   {
+        initweight = dryloopmass;
+   } 
+   
+   vTaskDelay(pdMS_TO_TICKS(1000)); //allow time to pass
+   // take weight again
+   /*
+    if (); // badadad needs work
     {
-        gpio_put(21,0); // turn off blue led
+        gpio_put(14,0);
     }
-    vTaskDelay(pdMS_TO_TICKS(500)); //500ms
+    */
 }
 
 
 int main()
-{
- bool waterstatus = false;
+{ 
+bool waterstatus = false;
+
+int ret; // unused?
+char buf[100]; // unused?
+char filename[] = "test3.csv";
+
  stdio_init_all();
  gpio_init(25); //onboard led
  gpio_set_dir(25,GPIO_OUT);
@@ -230,23 +313,7 @@ int main()
  gpio_set_dir(14,GPIO_OUT); // remove later
  gpio_set_dir(15,GPIO_OUT);
 
- /*
- uint slicenum = pwm_gpio_to_slice_num(14);
- pwm_clear_irq(slicenum); //clear pin interrupts
- 
- pwm_set_irq_enabled(slicenum, true); // enables interrupts
- irq_set_exclusive_handler(PWM_IRQ_WRAP, on_pwm_wrap);
- irq_set_enabled(PWM_IRQ_WRAP, true);
- //pwm_set_wrap(slicenum,500);
- //pwm_set_chan_level(slicenum, PWM_CHAN_A,500); // duty cycle = # / wrap point
-// gpio_set_dir(14,GPIO_OUT); 
-// pwm_set_enabled(slicenum, true);
-pwm_config config = pwm_get_default_config();
-    // Set divider, reduces counter clock to sysclock/this value
-    pwm_config_set_clkdiv(&config, 32.f);
-    // Load the configuration into our PWM slice, and set it running.
-    pwm_init(slicenum, &config, false); // starts off
-*/
+
 adc_init();// enable adc
 
 adc_gpio_init(26);
@@ -256,6 +323,7 @@ adc_gpio_init(26);
     gpio_put(25,1);
     
     gpio_put(14,0); // starts motor
+
     //gpio_put(14,1);  IGNORE PWM DISABLE
 
     //pwm_set_gpio_level(gpiopin, 1); // stops motor???
@@ -263,7 +331,8 @@ adc_gpio_init(26);
 // level sense
  gpio_init(18);
  gpio_set_dir(18,GPIO_IN);
- gpio_set_irq_enabled_with_callback(18,0x02,1,senseitr);
+ //gpio_pull_up(18); // added since thin sensor needs a pullup, remove for round sensor
+ gpio_set_irq_enabled_with_callback(18,0x01,1,senseitr);  // switched event masks from high 0x02 to low 0x01
 
 
 
@@ -303,9 +372,39 @@ uint16_t result; // moved out of loop, delete later??
 gpio_init(21); //blue led on
 gpio_set_dir(21,GPIO_OUT);
 
-
 gpio_init(20); //init amber led
 gpio_set_dir(20,GPIO_OUT);
+
+// sd section
+FATFS fs;
+FIL fil;
+FRESULT fr; 
+bool sd_ready = false;
+sleep_ms(5000);
+//initialize sd
+if(!sd_init_driver()){ 
+    gpio_put(21,1); // turn on blue led // REMOVE LATER
+    printf("SD driver Init Failed");
+}else {
+    fr = f_mount(&fs, "0:", 0); // Filesystem Mount
+    if (fr != FR_OK){
+    gpio_put(21,1); // turn on blue led /// REMOVE LATER
+    printf("SD Filesystem Mount Fail");
+    }else {
+        fr = f_open(&fil, filename, FA_OPEN_APPEND | FA_WRITE); // File Open
+        if (fr == FR_OK || fr == FR_EXIST) {
+            sd_ready = true; // SD card is ready to use
+        } else {
+            gpio_put(21,1); // turn on blue led /// REMOVE LATER
+            printf("Failed to open file: %s\n", FRESULT_str(fr));
+            //panic("f_open(%s) error: %s (%d)\n", filename, FRESULT_str(fr), fr); // panic might be stopping code, we'll comment it out for now
+            
+            
+
+        }
+        
+        }
+    }   
 
 
 initweightsetup(mass,opt); // call function for initial setup
@@ -357,7 +456,7 @@ if(scale_weight(&sc, &mass, &opt)) {
        printf("new maxweight : %f\n", maxweightstruct.ug);
       
     }
-    else if(mass.ug * 1.3 < maxweightstruct.ug && gpio_get(18) == false && mass.ug > 15 /* || pumptomaxweightflag true && wait if levelsense tripped timer is complete is true*/ ) // greater than 150 grams
+    else if(mass.ug * 1.3 < maxweightstruct.ug && gpio_get(18) != false  && mass.ug > 15 /* || pumptomaxweightflag true && wait if levelsense tripped timer is complete is true*/ ) // greater than 150 grams // changed pin 18 to != for npn sensor
     {
       bool pumptomax = true;
        mass_t lowestweight;
@@ -369,6 +468,7 @@ if(scale_weight(&sc, &mass, &opt)) {
         gpio_put(14,1); // turns on motor
         gpio_put(20,1);
 
+
        if(scale_weight(&sc, &mass, &opt)) { // gets weight
         double val;
         mass_get_value(&mass, &val);
@@ -379,11 +479,28 @@ if(scale_weight(&sc, &mass, &opt)) {
         datain = mass.ug;
         }
         printf("mass : %f\n",mass.ug); // test delete later
+        
+        if (sd_ready) // only save to sd if sd gets init
+        {
+        if (f_printf(&fil, "%f,\n",mass.ug) < 0) { // write to sd
+        printf("f_printf failed\n");
+        } else {
+            fr = f_sync(&fil); // sync file.
+            if (fr != FR_OK) {
+                // f_sync failed
+                printf("f_sync failed with error code %d\n", fr);
+             }
+        }
+        }
+
         if ( mass.ug >= maxweightstruct.ug /*add range of acceptable values*/)
         {
             pumptomax = false;
             gpio_put(14,0); // turn off pump
             gpio_put(20,0); // turn off amber led
+          
+            // This segment is depreciated water runout calculation/ newer RTOS version is replacing this
+            /*
             highestweight.ug = mass.ug; // used to get difference for water used CHANGE TO MASS_T IF DOESNT WORK
                 // do difference of water to get water used
                 waterused.ug = highestweight.ug - lowestweight.ug;
@@ -395,7 +512,10 @@ if(scale_weight(&sc, &mass, &opt)) {
                     gpio_put(21,1); // turn on blue led 
                     nowater = true; // bool for empty tank
                 }
+            */
             break; // break out of while loop
+            
+        
         }
         }
        
@@ -407,7 +527,9 @@ if(scale_weight(&sc, &mass, &opt)) {
         gpio_put(20,0);
         
         gpio_put(14,0);// drive pwm pin low
+
         // sleep_ms(100);
+
     }
  
 }
